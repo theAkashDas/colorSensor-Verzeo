c38d55c028fe962b5fa0ddc23e429986f200f72d
theAkashDas <akashdas250901@gmail.com>
2020-11-26 12:24:10 +0530

    testing2

--- ldr_verzeo/ldr_verzeo.ino
+++ ldr_verzeo/ldr_verzeo.ino
@@ -5,7 +5,7 @@ int valueAnalog = 0;     // result of reading the analog pin new
 void setup() {
   Serial.begin(9600);
   pinMode(13,OUTPUT);// sets serial port for communication
-  pinMode(12,OUTPUT);
+  pinMode(12,OUTPUT);// output
   pinMode(8,OUTPUT);
   pinMode(9,OUTPUT);
 
